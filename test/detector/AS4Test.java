64e75dc2a189921b239ba0cf6350f1055fd026e7
ANekhai <30994106+ANekhai@users.noreply.github.com>
2018-04-25 17:40:34 -0400

    Add files via upload

--- test/detector/AS4Test.java
+++ test/detector/AS4Test.java
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import structs.Genome;
+import structs.Grimm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class AS4Test {
     BPGraph graph;
     AS4 detector = new AS4();
+    String endLine = System.getProperty("line.separator");
 
     @BeforeEach
     void setUp() {
@@ -57,17 +62,75 @@ class AS4Test {
 
     }
 
+    //TODO: It seems like AS4 will detect AS2s as well... hmmm...
+
+//    @Test
+//    void testOnlyAS2s() {
+//        addGraphFromGenes("1", "2");
+//        addGraphFromGenes("1", "-2");
+//        addGraphFromGenes("1", "2"); // Throw away gene
+//
+//        detector.AS4(graph);
+//
+//        assertEquals(0, detector.numDetected);
+//    }
+
+
+    //TODO: These should be AS4s, gotta find the bug with them
     @Test
-    void testOnlyAS2s() {
-        addGraphFromGenes("1", "2");
-        addGraphFromGenes("1", "-2");
-        addGraphFromGenes("1", "2"); // Throw away gene
+    void testAS4noTriangles() throws IOException {
+        BufferedReader in = new BufferedReader(new StringReader(">One" + endLine + "1 2 4 3 @" +
+                endLine + ">Two" + endLine + "1 -2 -4 @" + endLine + "3 @" + endLine + ">Three" +
+                endLine + "-1 -4 -3 -2 @"));
+        ArrayList<Genome> genomes = Grimm.Reader.parseGRIMM(in);
+
+        for (Genome genome : genomes) {
+            ContractedGraph geneGraph = new ContractedGraph(genome);
+            graph.add(geneGraph);
+        }
 
         detector.AS4(graph);
 
-        assertEquals(0, detector.numDetected);
+        assertEquals(1, detector.numDetected);
+        // assertEquals(8, detector.foundSubgraphs.size());
+
     }
 
+    @Test
+    void testAS4withTriangle() throws IOException {
+        BufferedReader in = new BufferedReader(new StringReader(">One" + endLine + "1 2 4 3 @" +
+                endLine + ">Two" + endLine + "1 -2 @" + endLine + "3 4 @" + endLine + ">Three" +
+                endLine + "-1 -4 -3 2 @"));
+        ArrayList<Genome> genomes = Grimm.Reader.parseGRIMM(in);
 
+        for (Genome genome : genomes) {
+            ContractedGraph geneGraph = new ContractedGraph(genome);
+            graph.add(geneGraph);
+        }
+
+        detector.AS4(graph);
+
+        assertEquals(1, detector.numDetected);
+        assertEquals(8, detector.foundSubgraphs.size());
+
+    }
+
+    @Test
+    void testPentagonalAS4() throws IOException {
+        BufferedReader in = new BufferedReader(new StringReader(">One" + endLine + "1 4 3 2 @" +
+                endLine + ">Two" + endLine + "1 -2 @" + endLine + "-4 3 @" + endLine + ">Three" +
+                endLine + "-3 1 2 4 @"));
+        ArrayList<Genome> genomes = Grimm.Reader.parseGRIMM(in);
+
+        for (Genome genome : genomes) {
+            ContractedGraph geneGraph = new ContractedGraph(genome);
+            graph.add(geneGraph);
+        }
+
+        detector.AS4(graph);
+
+        assertEquals(1, detector.numDetected);
+        assertEquals(8, detector.foundSubgraphs.size());
+    }
 
 }
