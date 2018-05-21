0d0564c7ceeaad8922d4f6f12dbc02be429cb37b
ANekhai <30994106+ANekhai@users.noreply.github.com>
2018-05-21 15:44:58 -0400

    Add files via upload

---
+++ src/wrapper/TestWrapper.java
@@ -0,0 +1,57 @@
+package wrapper;
+
+import detector.Detector;
+import graphs.BPGraph;
+import solver.ExactSolver;
+import structs.Info;
+import structs.Parameters;
+import structs.SearchList;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+public class TestWrapper {
+    static String endLine = System.getProperty("line.separator");
+    static String tab = String.format("%c", '\t');
+
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.err.println("Please specify a GRIMM formatted genome file");
+        } else if (args.length > 1) {
+            System.err.println("This software currently only handles a single GRIMM file");
+        } else {
+
+            String filename = args[0];
+            BPGraph graph;
+
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(filename));
+
+                graph = new BPGraph(in);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error parsing file");
+            }
+
+            Detector detector = new Detector();
+            Parameters params = new Parameters();
+            Info info = new Info(params);
+            SearchList list = new SearchList();
+            ExactSolver solver = new ExactSolver();
+
+            System.out.println("Initial Bounds:" + endLine + "Lower: " + graph.getLowerBound() +
+                    " Upper: " + graph.getUpperBound());
+
+            int solution = solver.solve(graph, detector, info, list);
+
+            System.out.println("New Bound: " + solution + " Cycles Found: " + graph.getCycleNumber());
+
+        }
+    }
+
+}
